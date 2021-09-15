03ec7954e78e0ddd79658ccc67a6727e20642b9a
Chris Cassano <glitch0@gmail.com>
2021-09-14 20:53:41 -0700

    personal changed

--- src/App.js
+++ src/App.js
@@ -6,168 +6,169 @@ import { GoogleLogin } from "react-google-login";
 import LitJsSdk from "lit-js-sdk";
 
 function App() {
-    const gapi = window.gapi;
+  const gapi = window.gapi;
 
-    const [googleAccessKey, setGoogleAccessKey] = useState("");
-    const [litNodeClient, setLitNodeClient] = useState({});
-    const [link, setLink] = useState("");
-    const [shareLink, setShareLink] = useState("");
-    const [role, setRole] = useState(0);
-    const [modalOpen, setModalOpen] = useState(false);
-    const [token, setToken] = useState("");
-    const [accessControlConditions, setAccessControlConditions] = useState([]);
-    const [linkToDelete, setLinkToDelete] = useState("");
+  const [googleAccessKey, setGoogleAccessKey] = useState("");
+  const [litNodeClient, setLitNodeClient] = useState({});
+  const [link, setLink] = useState("");
+  const [shareLink, setShareLink] = useState("");
+  const [role, setRole] = useState(0);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [token, setToken] = useState("");
+  const [accessControlConditions, setAccessControlConditions] = useState([]);
+  const [linkToDelete, setLinkToDelete] = useState("");
 
-    function authenticate() {
-	return gapi.auth2
-	    .getAuthInstance()
-	    .grantOfflineAccess()
-	    .then(async (authResult) => {
-		if (authResult.code) {
-		    var litNodeClient = new LitJsSdk.LitNodeClient();
-		    await litNodeClient.connect();
-		    setLitNodeClient(litNodeClient);
-		    setToken(authResult.code);
-		} else {
-		    console.log("Error logging in");
-		}
-	    });
-    }
+  function authenticate() {
+    return gapi.auth2
+      .getAuthInstance()
+      .grantOfflineAccess()
+      .then(async (authResult) => {
+        if (authResult.code) {
+          var litNodeClient = new LitJsSdk.LitNodeClient();
+          await litNodeClient.connect();
+          setLitNodeClient(litNodeClient);
+          setToken(authResult.code);
+        } else {
+          console.log("Error logging in");
+        }
+      });
+  }
 
-    gapi.load("client:auth2", function () {
-	gapi.auth2.init({
-	    client_id:
-            "788318511760-576lveaqp1v3fk921tfo8k9rqf19ljp0.apps.googleusercontent.com",
-	    scope:
-            "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file",
-	});
+  gapi.load("client:auth2", function () {
+    gapi.auth2.init({
+      client_id:
+        "770315957372-em8a7ur994vogpn6dqhjq4olo0f8tcds.apps.googleusercontent.com",
+      scope:
+        "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file",
     });
+  });
 
-    const addToAccessControlConditions = (r) => {
-	setAccessControlConditions(accessControlConditions.concat(r));
+  const addToAccessControlConditions = (r) => {
+    setAccessControlConditions(accessControlConditions.concat(r));
+  };
+
+  const removeIthAccessControlCondition = (i) => {
+    let slice1 = accessControlConditions.slice(0, i);
+    let slice2 = accessControlConditions.slice(
+      i + 1,
+      accessControlConditions.length
+    );
+    setAccessControlConditions(slice1.concat(slice2));
+  };
+
+  const handleSubmit = () => {
+    const regex = /d\/(.{44})/g;
+    let id = link.match(regex)[0];
+    id = id.slice(2, id.length);
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        driveId: id,
+        role: role,
+        token: token,
+        accessControlConditions: accessControlConditions,
+      }),
     };
+    fetch("http://localhost:8080/api/share", requestOptions)
+      .then((response) => response.json())
+      .then(async (data) => {
+        console.log(data);
+        const accessControlConditions = data["authorizedControlConditions"];
+        const uuid = data["uuid"];
+        console.log(accessControlConditions);
+        const authSig = await LitJsSdk.checkAndSignAuthMessage({
+          chain: "polygon",
+        });
+        const resourceId = {
+          baseUrl: "http://localhost:8080",
+          path: "/l/" + uuid,
+          orgId: "",
+          role: role.toString(),
+          extraData: "",
+        };
+        const chain = "polygon";
+        console.log(accessControlConditions);
+        console.log("About to save");
+        await litNodeClient.saveSigningCondition({
+          accessControlConditions,
+          chain,
+          authSig,
+          resourceId,
+        });
+        setShareLink("http://localhost:8080/l/" + uuid);
+      });
+  };
 
-    const removeIthAccessControlCondition = (i) => {
-	let slice1 = accessControlConditions.slice(0, i);
-	let slice2 = accessControlConditions.slice(i + 1, accessControlConditions.length);
-	setAccessControlConditions(slice1.concat(slice2));
-    };
+  if (shareLink !== "") {
+    return <div>Share link: {shareLink}</div>;
+  }
 
-    const handleSubmit = () => {
-	const regex = /d\/(.{44})/g;
-	let id = link.match(regex)[0];
-	id = id.slice(2, id.length);
-	const requestOptions = {
-	    method: "POST",
-	    headers: { "Content-Type": "application/json" },
-	    body: JSON.stringify({
-		driveId: id,
-		role: role,
-		token: token,
-		accessControlConditions: accessControlConditions,
-	    }),
-	};
-	fetch("http://localhost:8080/api/share", requestOptions)
-	    .then((response) => response.json())
-	    .then(async (data) => {
-		console.log(data);
-		const accessControlConditions = data["authorizedControlConditions"];
-		const uuid = data["uuid"];
-		console.log(accessControlConditions);
-		const authSig = await LitJsSdk.checkAndSignAuthMessage({
-		    chain: "polygon",
-		});
-		const resourceId = {
-		    baseUrl: "http://localhost:8080",
-		    path: "/l/" + uuid,
-		    orgId: "",
-		    role: role,
-		    extraData: "",
-		};
-		const chain = "polygon";
-		console.log(accessControlConditions);
-		console.log("About to save");
-		await litNodeClient.saveSigningCondition({
-		    accessControlConditions,
-		    chain,
-		    authSig,
-		    resourceId,
-		});
-		setShareLink("http://localhost:8080/l/" + uuid);
-	    });
-    };
-
-    if (shareLink !== "") {
-	return <div>Share link: {shareLink}</div>;
-    }
-
-    if (token === "") {
-	return (
-		<div className="App">
-		<header className="App-header">
-		<button type="button" onClick={authenticate}>
-		Log in with Google
-            </button>
-		</header>
-		</div>
-	);
-    }
-
+  if (token === "") {
     return (
-	    <div className="App">
-	    <header className="App-header">
-            <p>Enter the link to the drive file below.</p>
-            <form>
-            <label for="drive-link">Drive Link</label>
-            <input
-        type="text"
-        name="drive-link"
-        id="drive-link"
-        onChange={(e) => setLink(e.target.value)}
-            />
+      <div className="App">
+        <header className="App-header">
+          <button type="button" onClick={authenticate}>
+            Log in with Google
+          </button>
+        </header>
+      </div>
+    );
+  }
 
-            <p>Added Access Control Conditions (click to delete)</p>
-            {accessControlConditions.map((r, i) => (
-		    <>
-		    <button onClick={() => removeIthAccessControlCondition(i)}>
-                    {JSON.stringify(r)}
-		</button>
-		    </>
-            ))}
-            <button type="button" onClick={() => setModalOpen(true)}>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Enter the link to the drive file below.</p>
+        <form>
+          <label for="drive-link">Drive Link</label>
+          <input
+            type="text"
+            name="drive-link"
+            id="drive-link"
+            onChange={(e) => setLink(e.target.value)}
+          />
+
+          <p>Added Access Control Conditions (click to delete)</p>
+          {accessControlConditions.map((r, i) => (
+            <>
+              <button onClick={() => removeIthAccessControlCondition(i)}>
+                {JSON.stringify(r)}
+              </button>
+            </>
+          ))}
+          <button type="button" onClick={() => setModalOpen(true)}>
             Add access control conditions
-        </button>
-            {modalOpen && (
-		    <ShareModal
-		show={false}
-		onClose={() => setModalOpen(false)}
-		sharingItems={[{ name: link }]}
-		onAccessControlConditionsSelected={(restriction) => {
-                    addToAccessControlConditions(restriction);
-                    setModalOpen(false);
-		}}
-		    />
-            )}
-            <br />
-            <label for="drive-role">Drive Role to share</label>
-            <select
-        name="drive-role"
-        id="drive-role"
-        onChange={(e) => setRole(parseInt(e.target.selectedIndex))}
-            >
+          </button>
+          {modalOpen && (
+            <ShareModal
+              show={false}
+              onClose={() => setModalOpen(false)}
+              sharingItems={[{ name: link }]}
+              onAccessControlConditionsSelected={(restriction) => {
+                addToAccessControlConditions(restriction);
+                setModalOpen(false);
+              }}
+            />
+          )}
+          <br />
+          <label for="drive-role">Drive Role to share</label>
+          <select
+            name="drive-role"
+            id="drive-role"
+            onChange={(e) => setRole(parseInt(e.target.selectedIndex))}
+          >
             <option value="read">Read</option>
             <option value="comment">Comment</option>
             <option value="write">Write</option>
-            </select>
-            <button type="button" onClick={handleSubmit}>
+          </select>
+          <button type="button" onClick={handleSubmit}>
             Get share link
-	</button>
-	    
-	</form>
-
-	</header>
-	    </div>
-    );
+          </button>
+        </form>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/Link.js
+++ src/Link.js
@@ -2,148 +2,149 @@ import { useState, useEffect } from "react";
 import LitJsSdk from "lit-js-sdk";
 
 function Link() {
-    const gapi = window.gapi;
+  const gapi = window.gapi;
 
-    const [conditionsFetched, setConditionsFetched] = useState(false);
-    const [error, setError] = useState("");
-    const [litNodeClient, setLitNodeClient] = useState({});
-    const [linkData, setLinkData] = useState([]);
-    const [email, setEmail] = useState("");
-    const [uuid, setUuid] = useState("");
+  const [conditionsFetched, setConditionsFetched] = useState(false);
+  const [error, setError] = useState("");
+  const [litNodeClient, setLitNodeClient] = useState({});
+  const [linkData, setLinkData] = useState([]);
+  const [email, setEmail] = useState("");
+  const [uuid, setUuid] = useState("");
 
-    useEffect(() => {
-	if (conditionsFetched === false) {
-	    const uuid = /[^/]*$/.exec(window.location.pathname)[0];
-	    setUuid(uuid);
-	    const requestOptions = {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify({ uuid: uuid }),
-	    };
-	    fetch("http://localhost:8080/api/conditions", requestOptions)
-		.then((response) => response.json())
-		.then(async (data) => {
-		    setConditionsFetched(true);
+  useEffect(() => {
+    if (conditionsFetched === false) {
+      const uuid = /[^/]*$/.exec(window.location.pathname)[0];
+      setUuid(uuid);
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ uuid: uuid }),
+      };
+      fetch("http://localhost:8080/api/conditions", requestOptions)
+        .then((response) => response.json())
+        .then(async (data) => {
+          setConditionsFetched(true);
 
-		    var litNodeClient = new LitJsSdk.LitNodeClient();
-		    await litNodeClient.connect();
-		    setLitNodeClient(litNodeClient);
-		    console.log(data["requirements"]);
-		    console.log(typeof data["role"]);
-		    setLinkData(data);
-		    console.log(data);
-		})
-		.catch((err) => {
-		    setError("Invalid link");
-		});
-	}
+          var litNodeClient = new LitJsSdk.LitNodeClient();
+          await litNodeClient.connect();
+          setLitNodeClient(litNodeClient);
+          console.log(data["requirements"]);
+          console.log(typeof data["role"]);
+          setLinkData(data);
+          console.log(data);
+        })
+        .catch((err) => {
+          setError("Invalid link");
+        });
+    }
+  }, []);
+
+  gapi.load("client:auth2", function () {
+    gapi.auth2.init({
+      client_id:
+        "788318511760-576lveaqp1v3fk921tfo8k9rqf19ljp0.apps.googleusercontent.com",
+      scope:
+        "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file",
+    });
+  });
+
+  async function provisionAccess() {
+    const chain = "polygon";
+    const resourceId = {
+      baseUrl: "http://localhost:8080",
+      path: "/l/" + uuid,
+      orgId: "",
+      role: linkData["role"].toString(),
+      extraData: "",
+    };
+
+    const authSig = await LitJsSdk.checkAndSignAuthMessage({ chain: chain });
+
+    const jwt = await litNodeClient.getSignedToken({
+      accessControlConditions: linkData["requirements"],
+      chain: chain,
+      authSig: authSig,
+      resourceId: resourceId,
     });
 
-    gapi.load("client:auth2", function () {
-	gapi.auth2.init({
-	    client_id:
-            "788318511760-576lveaqp1v3fk921tfo8k9rqf19ljp0.apps.googleusercontent.com",
-	    scope:
-            "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file",
-	});
-    });    
+    return jwt;
+  }
 
-    async function provisionAccess() {
-	const chain = "polygon";
-	const resourceId = {
-	    baseUrl: "http://localhost:8080",
-	    path: "/l/" + uuid,
-	    orgId: "",
-	    role: linkData["role"],
-	    extraData: "",
-	};
+  const handleDelete = () => {
+    return gapi.auth2
+      .getAuthInstance()
+      .grantOfflineAccess()
+      .then(async (authResult) => {
+        if (authResult.code) {
+          const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              uuid: uuid,
+              token: authResult.code,
+            }),
+          };
+          fetch("http://localhost:8080/api/delete", requestOptions)
+            .then((res) => {
+              if (res.status == 500) {
+                setError(
+                  "Error deleting link; were you the creator of this link?"
+                );
+              } else {
+                setError("Successfully deleted this link.");
+              }
+            })
+            .catch(() =>
+              setError(
+                "Error deleting link; were you the creator of this link?"
+              )
+            );
+        } else {
+          setError("Error logging in");
+        }
+      });
+  };
 
-	const authSig = await LitJsSdk.checkAndSignAuthMessage({ chain: chain });
+  const handleSubmit = () => {
+    provisionAccess().then((jwt) => {
+      const role = linkData["role"];
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, role, uuid, jwt }),
+      };
+      fetch("http://localhost:8080/api/share", requestOptions)
+        .then((response) => response.text())
+        .then((data) => console.log(data));
+    });
+  };
 
-	const jwt = await litNodeClient.getSignedToken({
-	    accessControlConditions: linkData["requirements"],
-	    chain: chain,
-	    authSig: authSig,
-	    resourceId: resourceId,
-	});
+  if (error !== "") {
+    return <div>{error}</div>;
+  }
 
-	return jwt;
-    }
+  if (conditionsFetched === false) {
+    return <div>Getting data...</div>;
+  } else {
+    return (
+      <div>
+        <label for="email-input">Enter your Google Account email here</label>
+        <input
+          type="text"
+          name="email-input"
+          id="email-input"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="button" onClick={handleSubmit}>
+          Request Access
+        </button>
 
-    const handleDelete = () => {
-	return gapi.auth2
-	    .getAuthInstance()
-	    .grantOfflineAccess()
-	    .then(async (authResult) => {
-		if (authResult.code) {
-		    const requestOptions = {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({
-			    uuid: uuid,
-			    token: authResult.code,
-			}),
-		    };
-		    fetch("http://localhost:8080/api/delete", requestOptions)
-			.then((res) => {
-			    if (res.status == 500) {
-				setError("Error deleting link; were you the creator of this link?");
-			    } else {
-				setError("Successfully deleted this link.")
-			    }
-			})			    
-			.catch(() =>
-			    setError(
-				"Error deleting link; were you the creator of this link?"
-			    )
-			);
-		} else {
-		    setError("Error logging in");
-		}
-	    });
-    };
-
-    const handleSubmit = () => {
-	provisionAccess().then((jwt) => {
-	    const role = linkData["role"];
-	    const requestOptions = {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify({ email, role, uuid, jwt }),
-	    };
-	    fetch("http://localhost:8080/api/share", requestOptions)
-		.then((response) => response.text())
-		.then((data) => console.log(data));
-	});
-    };
-
-    if (error !== "") {
-	return <div>{error}</div>;
-    }
-
-    if (conditionsFetched === false) {
-	return <div>Getting data...</div>;
-    }    
-    else {
-	return (
-		<div>
-		<label for="email-input">Enter your Google Account email here</label>
-		<input
-            type="text"
-            name="email-input"
-            id="email-input"
-            onChange={(e) => setEmail(e.target.value)}
-		/>
-		<button type="button" onClick={handleSubmit}>
-		Request Access
-            </button>
-
-		<button type="button" onClick={handleDelete}>
-		Delete this link
-            </button>
-		</div>
-	);
-    }
+        <button type="button" onClick={handleDelete}>
+          Delete this link
+        </button>
+      </div>
+    );
+  }
 }
 // Test link: http://testoauth.com/l/cce96c50-67f8-4b8c-a0a1-4dda0e1720a1
 // Test link no reqs: http://testoauth.com/l/5eb1da32-a04d-435b-bb93-9aa277b45b82
